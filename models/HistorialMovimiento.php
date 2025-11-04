e31792fe2ef77ac8126e37d6e753cfc7ac311064
Cristian Alvis <calvis@bancolombia.com.co>
2025-11-03 23:33:38 -0500

    Fix: Usar getDB() para tener acceso al método insert()

--- models/HistorialMovimiento.php
+++ models/HistorialMovimiento.php
@@ -10,7 +10,7 @@ class HistorialMovimiento {
     private $db;
 
     public function __construct() {
-        $this->db = Database::getInstance()->getConnection();
+        $this->db = getDB();
     }
 
     /**
